edf6e3c2052d78f0ff4ce5e6a256d94cfac799fd
PaulRobot <p229365@gmail.com>
2023-05-23 16:03:26 +0800

    我太難了 我真的不懂 https://ithelp.ithome.com.tw/articles/10217452

---
+++ A982122-linebot/LineBotConfig.cs
@@ -0,0 +1,8 @@
+﻿namespace A982122_linebot.Models;
+
+
+    public class LineBotConfig
+    {
+        public string channelSecret { get; set; }
+        public string accessToken { get; set; }
+    }
